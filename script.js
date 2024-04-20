1a38dba236fceba467efc42b99c2e16da448021d
Cesar Soto Castro <61519202+cs2005c@users.noreply.github.com>
2024-04-20 10:31:23 -0400

    Se entrega Atrasasdo el Desafio

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+ function pintar(){        
+         alert()
+        const ele = document.getElementById("ele1");
+
+        ele.addEventListener("click", function() {
+            this.style.backgroundColor = 'yellow';
+});
+ } 
+
+ pintar()
