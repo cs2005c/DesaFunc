fa4f0b7013a152144d7f6fa47b997e1f26c5f532
Cesar Soto Castro <61519202+cs2005c@users.noreply.github.com>
2024-04-22 21:01:11 -0400

    Se corrije Subida de Archivos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,73 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    /* Capturo los elementos */
+    const azul = document.getElementById('azul');
+    const rojo = document.getElementById('rojo');
+    const verde = document.getElementById('verde');
+    const amarillo = document.getElementById('amarillo');
+    const keyDiv = document.getElementById('key');
 
 
- function pintar(){        
-         alert()
-        const ele = document.getElementById("ele1");
+    azul.addEventListener('click', function() {
+        chgColor(azul, 'black');
+    });
 
-        ele.addEventListener("click", function() {
-            this.style.backgroundColor = 'yellow';
+    rojo.addEventListener('click', function() {
+        chgColor(rojo, 'black');
+    });
+
+    verde.addEventListener('click', function() {
+        chgColor(verde, 'black');
+    });
+
+    amarillo.addEventListener('click', function() {
+        chgColor(amarillo, 'black');
+    });
+
+    function chgColor(elemento, color) {
+        elemento.style.backgroundColor = color;
+    }
+
+    /*  Evaluo teclas presionadas */
+    document.addEventListener('keydown', function(event) {
+        let color;
+
+        switch (event.key) {
+            case 'a':
+                color = 'pink';
+                break;
+            case 's':
+                color = 'orange';
+                break;
+            case 'd':
+                color = 'lightblue';
+                break;
+            case 'q':
+                NewElementDiv('purple');
+                break;
+            case 'w':
+                NewElementDiv('gray');
+                break;
+            case 'e':
+                NewElementDiv('brown');
+                break;
+            default:
+                return; 
+        }
+
+        keyDiv.style.backgroundColor = color;
+    });
+
+
+    function NewElementDiv(colordiv) {
+        const DivGenerado= document.createElement('div');
+        DivGenerado.className = 'cajas';
+        DivGenerado.style.backgroundColor = colordiv;
+        DivGenerado.style.width = '200px';
+        DivGenerado.style.height = '200px';
+        DivGenerado.style.margin = '10px';
+        DivGenerado.style.display= 'inline-block';        
+
+        document.body.appendChild(DivGenerado); 
+    }
 });
- } 
-
- pintar()
